69c6031bcecc1dadc59db5e2daffd2fbddf81ae8
SayakSarkar02 <93426863+SayakSarkar02@users.noreply.github.com>
2023-05-21 12:57:02 +0530

    header tab div fix

--- components/MainBody.jsx
+++ components/MainBody.jsx
@@ -23,7 +23,7 @@ const MainBody = ({toggleTerminal, isTerminalOpen, setIsTerminalOpen}) => {
 
   return (
     <div  className=" text-vs-white-1 w-full flex flex-col justify-between h-[100vh]">
-      <div className="w-full h-10 bg-vs-gray-4"></div>
+      <div className="w-full h-10 mt-10 bg-vs-gray-4"></div>
       <div></div>
       <AnimatePresence>
         {isTerminalOpen && <motion.div initial={{y:300}} animate={{y:0}} exit={{y:300}} transition={{ type: 'tween', damping: 10 }}
