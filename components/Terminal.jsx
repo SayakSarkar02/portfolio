561c1b734e86122a32acaca876028ec0e30c21fd
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-05-21 19:37:39 +0530

    meet fix

--- components/Terminal.jsx
+++ components/Terminal.jsx
@@ -97,9 +97,10 @@ const Terminal = ({toggleTerminal}) => {
 
 
       {/* Meet Easter Egg */}
-      {meet && <div style={{ width: window.innerWidth, height:window.innerHeight }} className='absolute top-0 left-0 bg-[#1f2122] flex items-center justify-center'>
-        <div onClick={()=>setMeet(false)} className='absolute top-20 right-32 w-8 h-8 bg-red-700 rounded-full text-white text-xl leading-none cursor-pointer hover:bg-red-500 text-center pb-1 flex items-center justify-center font-sans'>x</div>
-          <Image src="/assets/meet.png" width={1350} height={100}></Image>
+      {meet &&
+      <div style={{ width: window.innerWidth, height:window.innerHeight }} className='absolute bottom-0 left-0 bg-[#1f2122] flex items-center justify-center'>
+        <div onClick={()=>setMeet(false)} className='absolute top-20 right-20 w-8 h-8 bg-red-700 rounded-full text-white text-xl leading-none cursor-pointer hover:bg-red-500 text-center pb-1 flex items-center justify-center font-sans'>x</div>
+        <img src="/assets/meet.png" className='h-[90%] self-end  object-cover'/>
       </div>}
 
 
