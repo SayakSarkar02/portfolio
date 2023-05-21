47ef6e1d9f2c9b98ac16475389b0a149dc522cb7
SayakSarkar02 <93426863+SayakSarkar02@users.noreply.github.com>
2023-05-21 20:27:52 +0530

    stop music button added

--- components/Terminal.jsx
+++ components/Terminal.jsx
@@ -14,7 +14,7 @@ const Terminal = ({toggleTerminal}) => {
     interrupt: false, // Allow playing multiple instances simultaneously (default: false)
   };
 
-  const [play] = useSound(kharahu, soundOptions);
+  const [play, { stop }] = useSound(kharahu, soundOptions);
 
   const router = useRouter();
   const inputRef = useRef(null);
@@ -117,7 +117,7 @@ const Terminal = ({toggleTerminal}) => {
       {/* Meet Easter Egg */}
       {meet &&
       <div style={{ width: window.innerWidth, height:window.innerHeight }} className='absolute bottom-0 left-0 bg-[#1f2122] flex items-center justify-center'>
-        <div onClick={()=>{setMeet(false);}} className='absolute top-20 right-20 w-8 h-8 bg-red-700 rounded-full text-white text-xl leading-none cursor-pointer hover:bg-red-500 text-center pb-1 flex items-center justify-center font-sans'>x</div>
+        <div onClick={()=>{setMeet(false);stop();}} className='absolute top-20 right-20 w-8 h-8 bg-red-700 rounded-full text-white text-xl leading-none cursor-pointer hover:bg-red-500 text-center pb-1 flex items-center justify-center font-sans'>x</div>
         <img src="/assets/meet.png" className='h-[90%] self-end  object-cover' alt="meet"/>
       </div>}
 
