17cde7d89a1bfa2cba6006e0e49f6f72e23fabcf
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-05-23 01:05:27 +0530

    temp commit remove

--- app/page.js
+++ app/page.js
@@ -11,6 +11,21 @@ import MidSec from '@components/MidSec'
 
 const Home = () => {
 
+  // const myinfo = [
+  //   {
+  //     name: "Sayak Sarkar",
+  //     age: 21,
+  //     gender: "Male",
+  //     address: "83/5, Suren Sarkar Road Kolkata-700010",
+  //     phone: "8420310125",
+  //     email: "sayaksarkar73@gmail.com",
+  //     linkedin: "https://www.linkedin.com/in/sayaksarkar02/",
+  //     twitter: "https://twitter.com/SayakSarkar02",
+  //     github: "https://github.com/SayakSarkar02",
+  //     instagram: "https://www.instagram.com/sayak__pvt/",
+  //   }
+  // ]
+
   const [isTerminalOpen, setIsTerminalOpen] = useState(true);
 
   const toggleTerminal = () => {
